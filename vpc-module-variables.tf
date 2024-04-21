2c5f33a03b58b1efac5debf754b5d72fef563b22
Ezyempire <156387732+Ezyempire@users.noreply.github.com>
2024-04-21 09:49:18 -0400

    Create vpc-module-variables.tf

---
+++ vpc-module-variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "pub_subnet" {
+  type = string
+}
+variable "priv_subnet" {
+  type = string
+}
+variable "pri_sub_availability_zone" {
+  type = string
+}
+
+variable "pub_sub_availability_zone" {
+  type    = string
+}
